bba527c6750991a8e4573e2bd443930760959de1
Orlando Martinez <orjamar@gmail.com>
2023-05-21 23:37:25 -0500

    Project completed

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-console.log("main js works")
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    const currencies = data.currencies;
 
-fetch("http://localhost:3000/currencies").then(response => response.json()).then(json => console.log(json))
+    const fromCurrencySelect = document.getElementById('from');
+    const toCurrencySelect = document.getElementById('to');
+    const amountInput = document.getElementById('amount');
+    const convertButton = document.getElementById('convertBtn');
+    const resultDiv = document.getElementById('result');
+
+    currencies.forEach(currency => {
+      const option1 = document.createElement('option');
+      option1.value = currency.code;
+      option1.text = `${currency.code} - ${currency.name}`;
+      fromCurrencySelect.appendChild(option1);
+
+      const option2 = document.createElement('option');
+      option2.value = currency.code;
+      option2.text = `${currency.code} - ${currency.name}`;
+      toCurrencySelect.appendChild(option2);
+    });
+
+    convertButton.addEventListener('click', () => {
+      const fromCurrency = fromCurrencySelect.value;
+      const toCurrency = toCurrencySelect.value;
+      const amount = amountInput.value;
+
+      const fromRate = currencies.find(currency => currency.code === fromCurrency).rate;
+      const toRate = currencies.find(currency => currency.code === toCurrency).rate;
+
+      const convertedAmount = (amount * toRate) / fromRate;
+      resultDiv.textContent = `Converted Amount: ${convertedAmount.toFixed(2)} ${toCurrency}`;
+    });
+
+    amountInput.addEventListener('change', () => {
+      convertButton.click();
+    });
+  });
